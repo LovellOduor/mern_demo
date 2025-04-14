ba1f464ac87b1259118b58d31d9c91dea64484ae
Lovell <lovell@Lovells-MacBook-Pro.local>
2025-04-14 21:18:24 +0300

    Mern stack demo

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+app.get(
+  '/',
+   (req, res) => {
+  res.sendFile("/Users/lovell/Desktop/exp/index.html")
+});
+
+app.get('/login', async (req, res) => {
+    var uri = "mongodb://localhost:27017/";
+  try{
+        // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+    const client = new MongoClient(uri,  {
+          serverApi: {
+          version: ServerApiVersion.v1,
+          strict: true,
+          deprecationErrors: true,
+      }
+  }
+  );
+
+  await client.connect();
+
+  var dbo = client.db("test");
+  var userData = { username:req.query.username, email:req.query.email };
+
+  await dbo.collection("users").insertOne(userData);
+
+  console.log("1 document inserted");
+
+  res.send("login success Username: " + req.query.username + " email: "+req.query.email);
+
+  await client.close();
+
+  }catch(e){
+    console.log(e);
+    res.send("Failed to insert user");
+    await client.close();
+  }
+
+});
+
+app.listen(port, 
+  () => {
+  console.log(`Example app listening on port ${port}`)
+});
